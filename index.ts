94f9b84d5061828f926df7b046f1e0048eedb54a
tino <vovthuy@gmail.com>
2021-08-27 13:34:21 -0700

    ping

---
+++ index.ts
@@ -0,0 +1,27 @@
+import DiscordJS, { Intents } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+// create bot client
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+
+// when bot is ready
+client.on('ready', () => {
+    console.log("Bot is ready!!")
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ping'){
+        message.reply({
+            content: 'pong',
+        })
+    }
+})
+
+client.login(process.env.TOKEN)
