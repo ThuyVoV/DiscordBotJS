ca9339313bc534f141fb4a79fc73c61e8789a2f0
tino <vovthuy@gmail.com>
2021-09-03 13:54:53 -0700

    commands

--- index.ts
+++ index.ts
@@ -14,13 +14,71 @@ const client = new DiscordJS.Client({
 // when bot is ready
 client.on('ready', () => {
     console.log("Bot is ready!!")
+
+    // test guild
+    const guildId = '504540136055242765'
+    const guild = client.guilds.cache.get(guildId)
+    let commands 
+
+    if (guild){
+        commands = guild.commands
+    }
+    else {
+        commands = client.application?.commands
+    }
+
+    commands?.create({
+        name: 'ping',
+        description: 'reply pong'
+    })
+
+    commands?.create({
+        name: 'add',
+        description: "add two nums",
+        options: [
+            {
+                name: 'num1',
+                description: "num1",
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            },
+            {
+                name: 'num2',
+                description: "num2",
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            }
+        ]
+    })
 })
 
 // ping pong
 client.on('messageCreate', (message) => {
-    if (message.content === 'ping'){
-        message.reply({
-            content: 'pong',
+    if (message.content === 'bam'){
+        message.channel.send(`${message.channel.valueOf()}`)
+    }   
+
+})
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()){
+        return
+    }
+
+    const {commandName, options} = interaction
+
+    if ( commandName === 'ping'){
+        interaction.reply({
+            content: "ping",
+            ephemeral: false
+        })
+    }
+    else if ( commandName === 'add'){
+        const num1 = options.getNumber('num1')!
+        const num2 = options.getNumber('num2')!
+
+        interaction.reply({
+            content: `${num1+num2}`
         })
     }
 })
