5b906e8238613ce0f99c4f6d1c56f566da859cc6
tino <vovthuy@gmail.com>
2021-09-10 15:45:37 -0700

    i guess i get reactions now

--- commands/randomCmd/pick.js
+++ commands/randomCmd/pick.js
@@ -9,16 +9,30 @@ module.exports = {
             option.setName('category')
                 .setDescription('The gif category')
                 .setRequired(true)
-                .addChoice('Funny', 'gif_funny')
-                .addChoice('Meme', 'gif_meme')
-                .addChoice('Movie', 'gif_movie')
+                .addChoice('Cat', 'cat')
+                .addChoice('Dog', 'dog')
+                .addChoice('Pig', 'pig')
             ),
 
 	async execute(interaction) {
+        const chose = interaction.options.getString('category')
         // const message = await interaction.reply('You can react with Unicode emojis!', { fetchReply: true })
-        await interaction.reply('You can react with Unicode emojis!');
+        await interaction.reply(`You chose: ${chose}!`);
+        await wait(1000)
+        await interaction.editReply(chose)
         const message = await interaction.fetchReply()
-        message.react("🐷")
+        console.log(chose, typeof chose)
+        //const emote = await interaction.client.emojis.cache.get("cat");
+        //const emote = await interaction.client.emojis.find(e => e.name === 'cat')
+        //const emote = await message.guild.emojis.cache.find(emoji => emoji.name == ":cat:")
+        //message.react(emote)
+        //const emote = message.guild.emojis.cache.find(e => e.name === "shuffle1")
+        const emote = interaction.client.emojis.cache.find(emote => emote.name === 'cat')
+        message.react(emote)
+        console.log(emote.name)
+        
+        // message.react("🐷")
+        // message.react("886004309429469254")
 
         //message.react('😄') 
 	},
--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ 
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] 
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+            Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS] 
 });
 client.commands = new Collection();
 
