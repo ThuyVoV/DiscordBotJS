eafe4b475834b4b0722b87e6e2c1c6a96b66c56d
Thuy Vo <vovthuy@gmail.com>
2021-09-10 21:14:26 -0700

    ok

--- commands/randomCmd/pick.js
+++ commands/randomCmd/pick.js
@@ -19,21 +19,15 @@ module.exports = {
         // const message = await interaction.reply('You can react with Unicode emojis!', { fetchReply: true })
         await interaction.reply(`You chose: ${chose}!`);
         await wait(1000)
-        await interaction.editReply(chose)
+        await interaction.editReply(`:${chose}:`)
         const message = await interaction.fetchReply()
         console.log(chose, typeof chose)
-        //const emote = await interaction.client.emojis.cache.get("cat");
-        //const emote = await interaction.client.emojis.find(e => e.name === 'cat')
-        //const emote = await message.guild.emojis.cache.find(emoji => emoji.name == ":cat:")
-        //message.react(emote)
-        //const emote = message.guild.emojis.cache.find(e => e.name === "shuffle1")
-        const emote = interaction.client.emojis.cache.first()
-        message.react(emote)
-        console.log(emote.name)
-        
-        // message.react("🐷")
-        // message.react("886004309429469254")
 
-        //message.react('😄') 
+        const emote = message.guild.emojis.cache.get("886004309429469254")
+        message.react(emote)
+        message.react("🐷") 
+        message.react("🐱")
+        message.react("🐶")
+        console.log(emote.name) 
 	},
 };
