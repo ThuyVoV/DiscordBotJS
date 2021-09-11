a6547c8a5c574941587a95c06d961a57268a9b4b
tino <vovthuy@gmail.com>
2021-09-11 14:50:32 -0700

    dog

--- commands/animals/animal.js
+++ commands/animals/animal.js
@@ -1,7 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const axios = require('axios')
 
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('animal')
@@ -16,17 +15,22 @@ module.exports = {
 
 	async execute(interaction) {
         let animal = await interaction.options.getString("category") 
-        console.log(animal)
-        await interaction.reply(animal)
+        //console.log(animal)
+        //await interaction.reply(animal)
         try{
-            let haha = await axios.get("https://api.thecatapi.com/v1/images/search")
-            console.log(haha.data[0].url)
-            await interaction.editReply(haha.data[0].url)
+            if (animal === 'cat'){
+                let haha = await axios.get("https://api.thecatapi.com/v1/images/search")
+                console.log(haha.data[0].url)
+                await interaction.reply(haha.data[0].url)
+            }
+            else if ( animal === 'dog'){
+                let haha = await axios.get("https://dog.ceo/api/breeds/image/random")
+                console.log(haha.data.message)
+                await interaction.reply(haha.data.message)
+            }
         }
         catch{
-            console.log('broken')
+            await interaction.reply("Something went wrong")
         }
-        // let haha = await fetch("https://api.thecatapi.com/v1/images/search")
-        // console.log(haha.json() )
 	},
 };
