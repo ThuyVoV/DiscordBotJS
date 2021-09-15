3d58d4d4125c4229f76b312bf71388eb250548c7
tino <vovthuy@gmail.com>
2021-09-15 15:17:47 -0700

    ok

--- commands/randomCmd/pick.js
+++ commands/randomCmd/pick.js
@@ -21,15 +21,47 @@ module.exports = {
         await wait(1000)
         await interaction.editReply(`:${chose}:`)
         const message = await interaction.fetchReply()
-        console.log(chose, typeof chose)
+
         
         const emojiName = "attackedpanda"
         const emote = interaction.client.emojis.cache.find(emojis => emojis.name === emojiName)
         //const emote = message.guild.emojis.cache.get("886004309429469254")
-        message.react(emote)
-        // message.react("🐷") 
-        // message.react("🐱")
-        // message.react("🐶")
-        console.log(emote.name) 
+        Promise.all([
+            message.react(emote),
+            //message.react("🐷"),
+            // message.react("🐱"),
+            // message.react("🐶"),
+        ])
+        
+        // for (const x of interaction.client.emojis.cache){
+        //     console.log("hi", x.name)
+        // }
+
+        //interaction.client.emojis.cache.each(x => console.log("hi", x.name, x.id))
+
+        try{
+            if (chose === 'dog'){
+                message.react("🐶")
+                const aa = message.reactions.cache.get("886004309429469254")
+                console.log(aa.name)
+                await message.reactions.removeAll()
+            }
+            else if (chose === 'cat'){
+                message.react("🐱")
+                console.log("in cat")
+                const something = message.reactions.cache.first()
+                // const something = await message.reactions.cache.find(emojis => emojis.id === "attackedpanda")
+                console.log(something)
+                //await message.reactions.cache.get('886004309429469254').remove()
+
+            }
+            else if (chose==="pig"){
+                message.react("🐷")
+            }
+        }
+        catch (error) {
+            //console.log("failed to remove reaction")
+            console.log("hmph", error)
+        }
 	},
 };
