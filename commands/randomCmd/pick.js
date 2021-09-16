063c87880686977f93ec44580ccdc772d58d5f1d
tino <vovthuy@gmail.com>
2021-09-16 15:51:30 -0700

    oinkmessage

--- commands/randomCmd/pick.js
+++ commands/randomCmd/pick.js
@@ -2,59 +2,66 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const wait = require('util').promisify(setTimeout);
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('pick')
-		.setDescription('pick choices')
-        .addStringOption( option => 
+    data: new SlashCommandBuilder()
+        .setName('pick')
+        .setDescription('pick choices')
+        .addStringOption(option =>
             option.setName('category')
                 .setDescription('The gif category')
                 .setRequired(true)
                 .addChoice('Cat', 'cat')
                 .addChoice('Dog', 'dog')
                 .addChoice('Pig', 'pig')
-            ),
+        ),
 
-	async execute(interaction) {
-        const chose = interaction.options.getString('category')
-        // const message = await interaction.reply('You can react with Unicode emojis!', { fetchReply: true })
-        await interaction.reply(`You chose: ${chose}!`);
-        await wait(1000)
-        await interaction.editReply(`:${chose}:`)
-        const message = await interaction.fetchReply()
+    async execute(interaction) {
+        try {
+            const chose = interaction.options.getString('category')
+            // const message = await interaction.reply('You can react with Unicode emojis!', { fetchReply: true })
+            await interaction.reply(`You chose: ${chose}!`);
+            await wait(1000)
 
-        
-        const emojiName = "attackedpanda"
-        const emote = interaction.client.emojis.cache.find(emojis => emojis.name === emojiName)
-        //const emote = message.guild.emojis.cache.get("886004309429469254")
-        Promise.all([
-            message.react(emote),
-            //message.react("🐷"),
-            // message.react("🐱"),
-            // message.react("🐶"),
-        ])
-        
-        // for (const x of interaction.client.emojis.cache){
-        //     console.log("hi", x.name)
-        // }
 
-        //interaction.client.emojis.cache.each(x => console.log("hi", x.name, x.id))
+            await interaction.editReply(`:${chose}:`)
+            await interaction.editReply(`WEEEWOOO`)
 
-        try{
-            if (chose === 'dog'){
+            let message = await interaction.fetchReply()
+
+
+            const emojiName = "attackedpanda"
+            const emote = interaction.client.emojis.cache.find(emojis => emojis.name === emojiName)
+            //const emote = message.guild.emojis.cache.get("886004309429469254")
+            Promise.all([
+                message.react(emote),
+                //message.react("🐷"),
+                // message.react("🐱"),
+                // message.react("🐶"),
+            ])
+
+            console.log("THIS IS THE MESSAGE CONTENT 1:", message.content)
+            // for (const x of interaction.client.emojis.cache){
+            //     console.log("hi", x.name)
+            // }
+
+            //interaction.client.emojis.cache.each(x => console.log("hi", x.name, x.id))
+
+            //try{
+            if (chose === 'dog') {
                 message.react("🐶")
                 const aa = message.reactions.cache.get("886004309429469254")
                 console.log(aa.name)
                 await message.reactions.removeAll()
             }
-            else if (chose === 'cat'){
+            else if (chose === 'cat') {
                 message.react("🐱")
                 console.log("in cat")
+                console.log("THIS IS THE MESSAGE CONTENT 2:", message.content)
                 const something = message.reactions.cache.first(2)
                 // const something = await message.reactions.cache.find(emojis => emojis.id === "attackedpanda")
-                console.log(something)
+                console.log("thi sis something", something)
                 //await message.reactions.cache.get('886004309429469254').remove()
             }
-            else if (chose==="pig"){
+            else if (chose === "pig") {
                 message.react("🐷")
             }
         }
@@ -62,5 +69,5 @@ module.exports = {
             //console.log("failed to remove reaction")
             console.log("hmph", error)
         }
-	},
+    },
 };
--- onMessageCreate/oinkMessage.js
+++ onMessageCreate/oinkMessage.js
@@ -1,12 +1,141 @@
 module.exports = {
     async heart(message) {
 
-        if (message.content.toLowerCase() === "oink"){
+        if (message.content.toLowerCase() === "oink") {
             await message.react("🐷")
+            await message.react("886004309429469254")
+
+
+            //const emote = 
+            const emote = message.reactions.cache.first(2)
+            console.log(emote.forEach((e, i) =>
+                console.log("hello", `@@this is i: ${i}@@`, e._emoji.name, e._emoji.id, e.count)
+            ))
+            console.log(emote)
+            //console.log(emote[0])
         }
 
-        //const emote = 
-        const emote = message.reactions.cache.first(2)
-        //console.log(emote.name)
-	},
+
+    },
 }
+
+/*
+emote[0]
+<ref *2> MessageReaction {
+    message: <ref *1> Message {
+        channelId: '662936355810050064',
+        guildId: '504540136055242765',
+        deleted: false,
+        id: '888175064636325919',
+        type: 'DEFAULT',
+        system: false,
+        content: 'oink',
+        author: User {
+            id: '90961398380625920',
+            bot: false,
+            system: false,
+            flags: [UserFlags],
+            username: 'oink',
+            discriminator: '1721',
+            avatar: '7ce4dc3d4b34c897b73f19b3d47c13ff'
+        },
+    pinned: false,
+    tts: false,
+    nonce: '888175056713154560',
+    embeds: [],
+    components: [],
+    attachments: Collection(0) [Map] {},
+    stickers: Collection(0) [Map] {},
+    createdTimestamp: 1631827836904,
+    editedTimestamp: null,
+    reactions: ReactionManager { message: [Circular *1] },
+    mentions: MessageMentions {
+        everyone: false,
+        users: Collection(0) [Map] {},
+        roles: Collection(0) [Map] {},
+        _members: null,
+        _channels: null,
+        crosspostedChannels: Collection(0) [Map] {},
+        repliedUser: null
+    },
+    webhookId: null,
+    groupActivityApplication: null,
+    applicationId: null,
+    activity: null,
+    flags: MessageFlags { bitfield: 0 },
+    reference: null,
+    interaction: null
+    },
+    me: true,
+    users: ReactionUserManager { reaction: [Circular *2] },
+    _emoji: ReactionEmoji {
+        animated: false,
+        name: '🐷',
+        id: null,
+        deleted: false,
+        reaction: [Circular *2]
+    },
+    count: 1
+}
+*/
+
+
+/*
+emote[1]
+<ref *2> MessageReaction {
+    message: <ref *1> Message {
+        channelId: '662936355810050064',
+        guildId: '504540136055242765',
+        deleted: false,
+        id: '888172099708014622',
+        type: 'DEFAULT',
+        system: false,
+        content: 'oink',
+        author: User {
+            id: '90961398380625920',
+            bot: false,
+            system: false,
+            flags: [UserFlags],
+            username: 'oink',
+            discriminator: '1721',
+            avatar: '7ce4dc3d4b34c897b73f19b3d47c13ff'
+        },
+        pinned: false,
+        tts: false,
+        nonce: '888172091763851264',
+        embeds: [],
+        components: [],
+        attachments: Collection(0) [Map] {},
+        stickers: Collection(0) [Map] {},
+        createdTimestamp: 1631827130010,
+        editedTimestamp: null,
+        reactions: ReactionManager { message: [Circular *1] },
+        mentions: MessageMentions {
+            everyone: false,
+            users: Collection(0) [Map] {},
+            roles: Collection(0) [Map] {},
+            _members: null,
+            _channels: null,
+            crosspostedChannels: Collection(0) [Map] {},
+            repliedUser: null
+        },
+        webhookId: null,
+        groupActivityApplication: null,
+        applicationId: null,
+        activity: null,
+        flags: MessageFlags { bitfield: 0 },
+        reference: null,
+        interaction: null
+    },
+    me: true,
+    users: ReactionUserManager { reaction: [Circular *2] },
+    _emoji: ReactionEmoji {
+        animated: false,
+        name: 'attackedpanda',
+        id: '886004309429469254',
+        deleted: false,
+        reaction: [Circular *2]
+    },
+    count: 1
+}
+*/
